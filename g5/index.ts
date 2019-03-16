c7a17c584160124d45ffd7e6b155f93c4a3ebabf
WMXPY <wm@mengw.io>
2019-03-15 20:48:25 -0500

    add init command

--- g5/index.ts
+++ g5/index.ts
@@ -8,7 +8,13 @@ import * as Commander from "commander";
 
 export default () => {
     Commander
-        .version('5.0.0', '-v, --version')
-        .parse(process.argv);
-};
+        .version('5.0.0', '-v, --version');
 
+    Commander
+        .command('init <template> <target>')
+        .action((template: string, action: string) => {
+            console.log(template, action);
+        });
+
+    Commander.parse(process.argv);
+};
---
+++ g5/init/init.ts
@@ -0,0 +1,10 @@
+/**
+ * @author WMXPY
+ * @namespace Init
+ * @description Init
+ */
+
+export const init = (template: string, target: string) => {
+
+
+};
